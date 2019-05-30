ce848c3b00465ee209e5477e963f082a352a8bfc
nevroje <joakim.berglind@gmail.com>
2019-05-30 14:06:09 +0200

    started adding learning logic for epoch and accuracy (wip)

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -6,3 +6,7 @@ from include.data import get_data_set
 from include.model import model,lr
 train_x, train_y = get_data_set("train")
 test_x, test_y = get_data_set("test")
+tf.set_random_seed(21)
+x, y, output, y_pred_cls, global_step, learning_rate = model
+global_accuracy = 0
+epoch_start = 0
